ad711fd34b9ad18123706a940c295f2d35d2b8fb
HenriBDev <henribdev@gmail.com>
2026-09-09 23:09:06 -0300

    🔧 log - Adding logs on intro-page

--- src/ui/adapter/input/app/components/pages/intro-page/intro-page.component.ts
+++ src/ui/adapter/input/app/components/pages/intro-page/intro-page.component.ts
@@ -1,7 +1,8 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { LogoComponent } from '@ui/adapter/input/app/components/shared/logo/logo.component';
+import { LoggerService } from '@ui/application/service/LoggerService';
 
 @Component({
     selector: 'app-intro-page',
@@ -12,9 +13,14 @@ import { LogoComponent } from '@ui/adapter/input/app/components/shared/logo/logo
     templateUrl: './intro-page.component.html',
     styleUrl: './intro-page.component.scss',
 })
-export class IntroPageComponent {
+export class IntroPageComponent implements OnInit {
 
 	private readonly router: Router = inject(Router);
+	private readonly logger: LoggerService = inject(LoggerService);
+
+	ngOnInit(): void {
+		this.logger.debug('IntroPageComponent initialized.');
+	}
 
 	onClickHandler = () => {
 		this.router.navigate(['/game-selection']);
